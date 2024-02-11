48e58e81b09f25def40cf261ca17b21bddc033ac
katelim215 <katelimosnero215@gmail.com>
2024-02-12 02:25:04 +0800

    task 1 complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryContainer = document.querySelector('.gallery');
+
+    function createGalleryItem(item) {
+        const listItem = document.createElement('li');
+        listItem.classList.add('gallery__item');
+
+        const link = document.createElement('a');
+        link.classList.add('gallery__link');
+        link.href = item.original;
+
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = item.preview;
+        image.alt = item.description;
+
+        link.appendChild(image);
+        listItem.appendChild(link);
+
+        return listItem;
+    }
+
+    function renderGallery() {
+        const galleryFragment = document.createDocumentFragment();
+        galleryItems.forEach(item => {
+            const galleryItem = createGalleryItem(item);
+            galleryFragment.appendChild(galleryItem);
+        });
+        galleryContainer.appendChild(galleryFragment);
+    }
+
+    renderGallery();
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+});
